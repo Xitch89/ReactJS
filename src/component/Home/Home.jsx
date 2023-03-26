2ced0b4b868955148ea056b687e687f59ccb7f0d
Xitch89 <xitch89@gmail.com>
2023-03-26 15:40:08 +0300

    Lesson 6 add weather API

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -7,13 +7,110 @@ import OurServices from '../OurServices/OurServices';
 import SocRef from '../SocRef/SocRef';
 import SubscribeBlock from '../SubscribeBlock/SubscribeBlock';
 import BottomBlock from '../BottomBlock/BottomBlock';
+import SearchWeather from '../weather/SearchWeather/SearchWeather';
+import WeatherInfo from '../weather/WeatherInfo/WeatherInfo';
+import classes from './Home.module.css';
+
+const API_KEY = '7b2c82371fe17f86708068b387c41dfd';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      temp: undefined,
+      city: undefined,
+      humidity: undefined,
+      sunrise: undefined,
+      wind: undefined,
+      error: undefined
+    };
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    
+    if (city) {
+      try {
+        const apiUrl = await
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=metric`);
+        const weatherData = await apiUrl.json();
+        
+        this.setState({
+          temp: weatherData.main.temp,
+          city: weatherData.name,
+          humidity: weatherData.main.humidity,
+          wind: weatherData.wind.speed,
+          error: undefined
+        });
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          this.setState({
+            city: undefined,
+            error: (
+              <div className={classes.weatherErrorGif}>
+                <p>The name of the city is incorrect</p>
+                <img 
+                  src="https://i.gifer.com/FZ26.gif" 
+                  alt="Gif 404" 
+                  width="100px" 
+                  height="75px"
+                />
+              </div>
+            )
+          });
+        } else {
+          this.setState({
+            city: undefined,
+            error: (
+              <div className={classes.weatherErrorGif}>
+                <p>The name of the city is incorrect</p>
+                <img 
+                  src="https://i.gifer.com/FZ26.gif" 
+                  alt="Gif 404" 
+                  width="100px" 
+                  height="75px"
+                />
+              </div>
+            )
+          });
+        }
+      }
+    } else {
+      this.setState({
+        city: undefined,
+        error: (
+          <div className={classes.weatherErrorGif}>
+            <p>Enter your city</p>
+            <img 
+              src="https://i.gifer.com/FZ26.gif" 
+              alt="Gif 404" 
+              width="100px" 
+              height="75px"
+            />
+          </div>
+        )
+      });
+    }
+  };
+
   render() {
+    const { 
+      temp, city, humidity, sunrise, wind, error 
+    } = this.state;
     return (
       <>
         <MainImage />
         <ExampleLayouts grayText="Our Core Layouts" yellowText="Template Pages" />
+        <SearchWeather weatherMethod={this.getWeather} />
+        <WeatherInfo 
+          temp={temp}
+          city={city}
+          humidity={humidity}
+          sunrise={sunrise}
+          wind={wind}
+          error={error}
+        />
         <Content />
         <BottomBlock />
         <ExampleLayouts grayText="Our Core Features" yellowText="Our Services" />
